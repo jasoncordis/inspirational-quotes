d3524fa0fe8807311ccb5dec894c459ac327798b
Avineet Kumar <avineetkumar4@gmail.com>
2019-01-02 02:01:33 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn inspirational_quotes:app
---
+++ inspirational_quotes.py
@@ -0,0 +1,57 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import pandas as pd
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+bot=ChatBot('Inspiration Quotes')
+bot.set_trainer(ListTrainer)
+df=pd.read_csv('quotes_data.csv',encoding ='latin1')
+new = df["hrefs"].str.split("src=t_", n = 1, expand = True)
+df['quotes_type']=new[1]
+author = df["lines"].str.split(".-", n = 1, expand = True)
+df["quotes_lines"]=author[0]
+dataset=df.drop(['lines', 'hrefs'], axis=1)
+df_new = dataset.groupby('quotes_type').agg({'quotes_lines': ', '.join}).reset_index()
+final_df=df_new[['quotes_type','quotes_lines']]
+question=list(final_df['quotes_type'])
+for index, row in final_df.iterrows():
+    ques=row['quotes_type']
+    ans=row['quotes_lines']
+    bot.train([ques, ans])
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    userText=userText.lower()
+    if (userText=="hello" or userText=="Hello"):
+        str_question=','.join(question)
+        sentence = "Hello,Please type your quotes name listed " + str_question.upper()
+    elif (userText == "bye" or userText == "Bye"):
+        sentence = "Bye..See you again"
+    elif userText  not in question:
+        sentence=userText +' quotes is not available'
+    else:
+         data= str(bot.get_response(userText))
+         sentence = data.replace(", ", ".\n")
+    return  sentence
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+while True:
+    message=input('You:')
+    if message.strip() !='Bye':
+
+        reply=bot.get_response(message)
+        print('Chatbot :',reply)
+    if message.strip()=='Bye':
+        print('Chatbot : Bye')
+        break
