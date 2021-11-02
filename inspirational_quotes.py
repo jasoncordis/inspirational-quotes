21ebaf9dc6aa5aabac4679b49132cfc6609c3a56
jasoncordis <cordisjason@gmail.com>
2021-11-02 02:36:29 -0700

    Update inspirational_quotes.py

--- inspirational_quotes.py
+++ inspirational_quotes.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 bot=ChatBot('Inspiration Quotes', 
             storage_adapter='chatterbot.storage.SQLStorageAdapter',
-            database_uri='sqlite:///database400.sqlite3')
+            database_uri='sqlite:///database1.sqlite3')
 bot.set_trainer(ListTrainer)
 df=pd.read_csv('quotes_data.csv',encoding ='latin1')
 new = df["hrefs"].str.split("src=t_", n = 1, expand = True)
