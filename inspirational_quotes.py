dd692594566fd8271c61491dcc117b627017953f
jasoncordis <cordisjason@gmail.com>
2021-11-02 13:45:27 -0800

    Update inspirational_quotes.py

--- inspirational_quotes.py
+++ inspirational_quotes.py
@@ -9,6 +9,7 @@ bot=ChatBot('Inspiration Quotes')
 bot.set_trainer(ListTrainer)
 bot.train(["Who is Saul Goodman?", "Saul Goodman is… He’s the last line of defense for the little guy. Are you getting sold down the river? He’s a life raft. You getting stepped on, he’s a sharp stick. You got Goliath on your back, Saul’s the guy with the slingshot. He’s a righter of wrongs. He’s friend to the friendless. That’s Saul Goodman."])
 bot.train(["Tell me a joke.", "You know why God made snakes before he made lawyers? He needed the practice."])
+bot.train(["What's your real name?", "My real name’s McGill. The Jew thing I just do for the homeboys. They all want a pipe-hitting member of the tribe, so to speak."])
 
 @app.route("/")
 def home():
