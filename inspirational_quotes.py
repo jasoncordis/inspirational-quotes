9e488f0be7f58db8c6085d628f5e469569509fae
jasoncordis <cordisjason@gmail.com>
2021-11-01 21:47:20 -0800

    Update inspirational_quotes.py

--- inspirational_quotes.py
+++ inspirational_quotes.py
@@ -5,17 +5,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-bot=ChatBot('Saul Goodman')
+bot=ChatBot('Inspirational Quotes')
 trainer = ListTrainer(bot)
 trainer.train([
     "Saul Goodman is… He’s the last line of defense for the little guy. Are you getting sold down the river? He’s a life raft. You getting stepped on, he’s a sharp stick. You got Goliath on your back, Saul’s the guy with the slingshot. He’s a righter of wrongs. He’s friend to the friendless. That’s Saul Goodman."
     "You know why God made snakes before he made lawyers? He needed the practice.",
 ])
 
-trainer.train([
-    "Only two things I know about Albuquerque. Bugs Bunny should’ve taken a left turn there. And, give me a hundred tries, I’ll never be able to spell it.",
-    "My real name’s McGill. The Jew thing I just do for the homeboys. They all want a pipe-hitting member of the tribe, so to speak…",
-])
 
 @app.route("/")
 def home():
