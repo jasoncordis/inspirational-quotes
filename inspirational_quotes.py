bf849377def11e6a9edbde4ad73b510310124b2d
jasoncordis <cordisjason@gmail.com>
2021-11-02 13:41:19 -0800

    Update inspirational_quotes.py

--- inspirational_quotes.py
+++ inspirational_quotes.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 bot=ChatBot('Inspiration Quotes')
 bot.set_trainer(ListTrainer)
-bot.train(["saul", "goodman"])
+bot.train(["Who is Saul Goodman?", "Saul Goodman is… He’s the last line of defense for the little guy. Are you getting sold down the river? He’s a life raft. You getting stepped on, he’s a sharp stick. You got Goliath on your back, Saul’s the guy with the slingshot. He’s a righter of wrongs. He’s friend to the friendless. That’s Saul Goodman."])
 
 @app.route("/")
 def home():
