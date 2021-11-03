901d9b323ee276b42ec121fe8d160eb316234b0c
jasoncordis <cordisjason@gmail.com>
2021-11-03 12:23:03 -0800

    Update inspirational_quotes.py

--- inspirational_quotes.py
+++ inspirational_quotes.py
@@ -1,12 +1,16 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+from chatterbot.storage import StorageAdapter
 import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-bot=ChatBot('Inspiration Quotes')
-bot.set_trainer(ListTrainer)
+bot=ChatBot('Inspiration Quotes',     storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+    logic_adapters=[
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='mongodb+srv://user:csc675@cluster0.7udau.mongodb.net/libraryapp?retryWrites=true&w=majority')
 
 @app.route("/")
 def home():
