4430cd726d69c39db47423d2940e83f972b37cd0
jasoncordis <cordisjason@gmail.com>
2021-11-03 21:46:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,9 @@ bot=ChatBot('Saul Goodman',     storage_adapter='chatterbot.storage.MongoDatabas
     ],
     database_uri='mongodb+srv://user:csc675@cluster0.7udau.mongodb.net/libraryapp?retryWrites=true&w=majority')
 bot.set_trainer(ListTrainer)
-bot.train(["This person might snitch on me and get me in trouble!", "Have you given any thought on sending him on a trip to Belize?"])
-bot.train(["Should I take this opportunity?","You walk by a $20 on the sidewalk? No, you pick it up."])
-bot.train(['I had a mistrial', 'Two sweetest words in the English language, "miss," "trial."'])
-bot.train(['I ran out of ideas', 'There’s always another play.'])
-bot.train(['I want to save my dignity, reputation, and honor', "You wanna save your dignity? You’re gonna have to fight."])
-bot.train(["I'm early or late to this meeting at this time", "Never bad to be early, except in death and taxes and… some other things."])
-bot.train(["I need to work on my appearance, dress nicer, stylish, and look successful.", "Got to look successful to be successful."])
-
+bot.train(["What do you know about scientists", "Scientists love lasers."])
+bot.train(["I made a big mistake","That's what the kids call 'epic fail.'"])
+bot.train(["Can you do money lanudering for me?", "Believe me, money laundering ain't what it used to be. God, do I miss the '80s."])
            
 @app.route("/")
 def home():
