ed67ab451cb8f4f75a15c9735404834bbc811756
jasoncordis <cordisjason@gmail.com>
2021-11-03 18:02:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,18 @@ bot=ChatBot('Saul Goodman',     storage_adapter='chatterbot.storage.MongoDatabas
     ],
     database_uri='mongodb+srv://user:csc675@cluster0.7udau.mongodb.net/libraryapp?retryWrites=true&w=majority')
 bot.set_trainer(ListTrainer)
-bot.train(["Tell me about Albuquerque.", "Only two things I know about Albuquerque. Bugs Bunny should’ve taken a left turn there. And, give me a hundred tries, I’ll never be able to spell it."])
-bot.train(["How can I trick people?", "If you're committed enough, you can make any story work. I once told a woman I was Kevin Costner, and it worked because I believed it."])
-bot.train(["Do you work with old people?", "FYI? Old people adore me."])
-    
+bot.train(["What is your opinion on this issue?", "The path to justice is rarely smooth."])
+bot.train(["Do you play music?", "Oh, I tried to learn guitar in high school, but then I decided there were easier ways to get girls."])
+bot.train(["I got in trouble. I had a bad day.", "You act like you’re the first guy this ever happened to. I caught my second wife screwing my stepdad, okay? It’s a cruel world, grow up.])
+bot.train(["Goodbye", "So if you wanna make more money and, uh, keep the money that you make... better call Saul!"])
+bot.train(["You owe me money.", "It wasn't me, it was Ignacio! He's the one!"])
+bot.train(["What is your opnion?", "I'm not saying it's not bad. It's bad. But it could be worse."])
+bot.train(["Hello", "Hi. I'm Saul Goodman. Did you know that you have rights? The Constitution says you do. And so do I. I believe that until proven guilty, every man, woman, and child in this country is innocent. And that's why I fight for you, Albuquerque!"])
+bot.train(["I've seen better acting in an epileptic whorehouse.", "Is that like the one your mom works at? Uh, is she still offering the two-for-one discount?"])
+bot.train(["How's Skyler?", "Walter never told me how lucky he was. Clearly his taste in women is the same as his taste in lawyers : only the very best... with just a right amount of dirty!"])
+bot.train(["How's Walt and Jesse?", "Walt and Jesse? If I ever get anal polyps, I'll know what to name them."])
+bot.train(["How do you know Gus?", "Let’s just say I know a guy… who knows a guy… who knows another guy."])
+           
 @app.route("/")
 def home():
     return render_template("index.html")
