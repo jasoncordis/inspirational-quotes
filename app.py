6bf6d8c01582a0ef105ad31ce38ee53cb2f95af2
jasoncordis <cordisjason@gmail.com>
2021-11-08 18:33:23 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,10 @@ app = Flask(__name__)
 bot=ChatBot('Saul Goodman',     storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
     logic_adapters=[
         'chatterbot.logic.BestMatch'
+         tie_breaking_method="random_response"
     ],
     database_uri='mongodb+srv://user:csc675@cluster0.7udau.mongodb.net/libraryapp?retryWrites=true&w=majority')
 bot.set_trainer(ListTrainer)
-bot.train(["How is Howard?", "Howard is a teensy, tiny man in a teensy-weensy little bubble.  You know why I didn't take his job? Because it was too small! I don't care about it! I was nothing to me! I was a bacterium! I travel in worlds he can't even imagine! He can't conceive of what I'm capable of! I'm so far beyond him! I'm like a god in human clothing! Lighting bolts shoot from my fingertips!"])
-bot.train(["What's your opinion on this politician?", "Some people are immune to good advice."])
-bot.train(["Tell me a joke.", "Why do they bury lawyers under 20 feet of dirt? Because deep down, they’re really good people."])
-bot.train(["Tell me a joke.", "Why do they bury lawyers under 20 feet of dirt? Because deep down, they’re really good people."])
-bot.train(["Tell me a joke.", "Why do they bury lawyers under 20 feet of dirt? Because deep down, they’re really good people."])
-bot.train(["Tell me a joke.", "Why do they bury lawyers under 20 feet of dirt? Because deep down, they’re really good people."])
 
 @app.route("/")
 def home():
